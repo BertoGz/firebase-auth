6c880df3b64cf7227f833ac284bd740c565d2053
bertogz <bertogzlx@gmail.com>
2022-10-26 12:43:58 -0700

    add initial components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Input } from "./Components/Input";
+import { HiOutlineMail } from "react-icons/hi";
+import { AiFillPhone } from "react-icons/ai";
 function App() {
+  const isHappy = false;
+  const [isAuth, setIsAuth] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex place-items-center justify-center bg-red-300 h-screen">
+      <div className="bg-slate-50 inline-flex flex-col space-y-2 p-2 mb-[50%] shadow-lg rounded">
+        {false && (
+          <button
+            class={`py-2 px-4 bg-blue-400 font-bold rounded ${
+              isHappy ? "text-white" : "text-black"
+            }`}
+          >
+            Find friends
+          </button>
+        )}
+        <div className="inline-flex flex-row">
+          <AiFillPhone className="text-emerald-400" />
+          <h1 className="text-slate-600 font-bold">HowAreU</h1>
+        </div>
+        <h1 className="text-3xl text-slate-600 font-bold text-center">
+          Register
+        </h1>
+        <Input
+          placeholder="Enter your Email"
+          startAdornment={<HiOutlineMail />}
+        />
+        <Input
+          placeholder="Enter your Password"
+          startAdornment={<HiOutlineMail />}
+        />
+        <h1 className="text-slate-500">
+          Already a user?{" "}
+          <a href="/user" className="text-blue-500">
+            Login
+          </a>
+        </h1>
+      </div>
     </div>
   );
 }
