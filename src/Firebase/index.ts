8d85b694918f0456e7fbf7807c32091f083154e1
bertogz <bertogzlx@gmail.com>
2022-10-28 10:11:19 -0700

    update url

--- src/Firebase/index.ts
+++ src/Firebase/index.ts
@@ -60,7 +60,7 @@ export const FirebaseActions = {
     const actionCodeSettings = {
       // URL you want to redirect back to. The domain (www.example.com) for this
       // URL must be in the authorized domains list in the Firebase Console.
-      url: "https://fir-auth-app-e0044.firebaseapp.com",
+      url: "https://fir-auth-app-e0044.web.app",
       // This must be true.
       handleCodeInApp: true,
     };
